b3073ccda24df9bd39d724a4630151c66a8547c9
David Mirabito <david.mirabito@gmail.com>
2016-06-01 10:05:39 +1000

    First pass at writing objb files.

--- main.cpp
+++ main.cpp
@@ -15,27 +15,28 @@ main(int argc, char *argv[])
 
     const char *filename = argc > 1? argv[1] : "gt86.obja";
     ifstream fin(filename, ios::in);
-    if (!fin) {
-        cout << "Unable to open " << filename  << ": " << strerror(errno) << endl;
-        return -1;
-    }
+    if (!fin) throw StackException("Unable to open input: " + std::string(strerror(errno)));
 
-    {  Timer timer("Objfile load time");
-       auto obj = ObjFile(fin);
+    Timer timer("Objfile load time");
+    auto obj = ObjFile(fin, filename);
 
-       if (0) {
-           cout << "Vertices : " << obj.positions.size() << std::endl;
-           cout << "Normals  : " << obj.normals.size() << std::endl;
-           cout << "TexCoords: " << obj.coords.size() << std::endl;
-           cout << endl;
+    ofstream fout("output.out", ios::out);
+    if (!fout) throw StackException("Cannot open output: " + std::string(strerror(errno)));
 
-           for(auto &g: obj.groups) {
-               std::cout << "Group: " << g.name << std:: endl;
-               std::cout << "       Faces:" << g.faces.size() << std::endl;
-               std::cout << "       idx_Base :  " << g.base_idx <<std::endl;
-               std::cout << "       idx_Count:  " << g.count <<std::endl;
-           }
-       }
+    obj.ToBin(fout);
+
+    if (0) {
+        cout << "Vertices : " << obj.positions.size() << std::endl;
+        cout << "Normals  : " << obj.normals.size() << std::endl;
+        cout << "TexCoords: " << obj.coords.size() << std::endl;
+        cout << endl;
+
+        for(auto &g: obj.groups) {
+            std::cout << "Group: " << g.name << std:: endl;
+            std::cout << "       Faces:" << g.faces.size() << std::endl;
+            std::cout << "       idx_Base :  " << g.base_idx <<std::endl;
+            std::cout << "       idx_Count:  " << g.count <<std::endl;
+        }
     }
 
     return 0;
--- obj.cpp
+++ obj.cpp
@@ -4,14 +4,14 @@
 #include "obj.h"
 #include "util.h"
 
-#include "VertexCache.h"
 
 using std::cout;
 using std::endl;
 
 
-ObjFile::ObjFile(std::ifstream &fin)
+ObjFile::ObjFile(std::ifstream &fin, const std::string &name)
 {
+    this->name = name;
 
 
 #define SKIP_WS(_f)  _f>>std::ws
@@ -53,13 +53,13 @@ ObjFile::ObjFile(std::ifstream &fin)
         }
     }
 
-    VertexCache vc;
+    vc = std::unique_ptr<VertexCache>(new VertexCache); 
     for (auto &g :groups) {
-        g.base_idx = vc.count();
+        g.base_idx = vc->count();
         for (auto &f : g.faces) {
-            vc.feed(f.vertices[0]);
-            vc.feed(f.vertices[1]);
-            vc.feed(f.vertices[2]);
+            vc->feed(f.vertices[0]);
+            vc->feed(f.vertices[1]);
+            vc->feed(f.vertices[2]);
             g.count+=3;
         }
     }
@@ -130,5 +130,132 @@ void ObjFile::AddTexcoord(const std::string &line)
     float s = strtof( end, &end );
     float t = strtof( end, &end );
     coords.push_back(pair(s,t));
+}
+
+void ObjFile::ToASCII(std::ofstream &fout)
+{
+    int i = 0;
+    for (auto p: vc->buffer) {
+        fout << "Vertex: "<< ++i << std::endl;
+        int p_idx = std::get<0>(p)-1;
+        int t_idx = std::get<1>(p)-1;
+        int n_idx = std::get<2>(p)-1;
+        fout << "  p" << p_idx<<": "<< positions[p_idx] << std::endl;
+        fout << "  t" << t_idx<<": "<< positions[t_idx] << std::endl;
+        fout << "  n" << n_idx<<": "<< positions[n_idx] << std::endl;
+    };
+
+
+    fout << std::endl;
+    fout << "Indices:" << std::endl;
+    i=0;
+    for (auto x: vc->indices) {
+        i++;
+        fout << "  V" << x;
+        if (i & ((i%3)==0)) fout<<std::endl;
+        else fout<<"\t";
+    }
+}
+
+static inline void
+do_write(std::ofstream &fp, std::string str, int len)
+{
+    str.resize(len, '\0');
+    fp.write(str.c_str(), len);
+}
+
+
+template<typename T>
+static inline void
+do_write(std::ofstream &fp, T val)
+{
+    auto ptr = reinterpret_cast<const char *>(&val);
+    fp.write(ptr, sizeof(val));
+}
+
+static inline void
+do_write(std::ofstream &fp, triple &t)
+{
+    do_write<float>(fp, std::get<0>(t));
+    do_write<float>(fp, std::get<1>(t));
+    do_write<float>(fp, std::get<2>(t));
+}
+
+/* DataType */
+#define GL_BYTE                           0x1400
+#define GL_UNSIGNED_BYTE                  0x1401
+#define GL_SHORT                          0x1402
+#define GL_UNSIGNED_SHORT                 0x1403
+#define GL_INT                            0x1404
+#define GL_UNSIGNED_INT                   0x1405
+#define GL_FLOAT                          0x1406
+#define GL_DOUBLE                         0x140A
+
+
+void ObjFile::ToBin(std::ofstream &fout)
+{
+    const uint32_t magic = 0x0B1EC701;
+    const uint16_t major = 0x0001;
+    const uint16_t minor = 0x0001;
+    const uint16_t index_type = GL_UNSIGNED_INT;
+    const uint16_t index_count = vc->indices.size();
+    const uint32_t buffsize = vc->buffer.size() * 3 * sizeof(float);
+
+    const uint32_t num_attrs = 2;
+    const uint32_t num_groups = groups.size();
+
+    // header
+    do_write<uint32_t>(fout, magic);
+    do_write<uint16_t>(fout, major);
+    do_write<uint16_t>(fout, minor);
+    do_write<uint16_t>(fout, index_type);
+    do_write<uint16_t>(fout, index_count);
+    do_write<uint32_t>(fout, buffsize);
+    do_write(fout, name, 16);
+    do_write<uint32_t>(fout, num_attrs);
+    do_write<uint32_t>(fout, num_groups);
+
+    const uint32_t float1_size = 1 * sizeof(float);
+    const uint32_t float3_size = 3 * sizeof(float);
+
+    // Attr1: Position
+    do_write(fout, "position", 16);
+    do_write<uint32_t>(fout, float3_size); // stride
+    do_write<uint32_t>(fout, 0); // offset
+    do_write<uint32_t>(fout, GL_FLOAT); // elem_type
+    do_write<uint32_t>(fout, 3); // elem_count
+
+    // Attr2: Normal
+    do_write(fout, "colour", 16);
+    do_write<uint32_t>(fout, float3_size); // stride
+    do_write<uint32_t>(fout, float1_size); // offset
+    do_write<uint32_t>(fout, GL_FLOAT); // elem_type
+    do_write<uint32_t>(fout, 3); // elem_count
+
+    // Groups
+    for(auto g: groups) {
+        do_write(fout, g.name, 16);
+        do_write<uint16_t>(fout, g.base_idx);
+        do_write<uint16_t>(fout, g.count);
+    }
+
+    for (auto i: vc->indices) {
+        if (index_type==GL_UNSIGNED_INT)
+            do_write<uint32_t>(fout, i);
+        else if (index_type==GL_UNSIGNED_SHORT)
+            do_write<uint16_t>(fout, i);
+        else if (index_type==GL_UNSIGNED_BYTE)
+            do_write<uint8_t>(fout, i);
+    }
+
+    for (auto v: vc->buffer) {
+        const unsigned int p_idx = std::get<0>(v)-1;
+        const unsigned int t_idx = std::get<1>(v)-1;
+        const unsigned int n_idx = std::get<2>(v)-1;
+        do_write(fout, positions[p_idx]);
+        do_write(fout, normals[n_idx]);
+        do_write(fout, coords[t_idx]);
+
+    }
 }
 
