5ad9f37a666c04c83d86dbc79113d8a36db67684
David <david.mirabito@gmail.com>
2016-06-16 08:39:59 +1000

    Re-use vertices if possible.
    Uses dumb linear search of existing verts (could use map instead?)
    Uses dumb component-wise test for equality (hash?)

--- Vertex.h
+++ Vertex.h
@@ -17,10 +17,25 @@ struct Vertex {
         has_tc = true;
     }
 
+    bool operator==(const Vertex& other)
+    {
+        if (pos != other.pos) return false;
+
+        if (has_normal != other.has_normal) return false;
+        if (has_normal && (normal != other.normal)) return false;
+
+        if (has_tc != other.has_tc) return false;
+        if (has_tc && (tc != other.tc)) return false;
+
+        return true;
+    }
+
     float3 pos, normal;
     float2 tc;
 
     bool has_normal;
     bool has_tc;
 };
+
+
 
